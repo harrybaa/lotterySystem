bfe33b6966b37b04739321e01cef1f8aca8399dc
Harry Liu <harrybaa@gmail.com>
2017-02-28 12:49:03 -0800

    Frame Work
    And skeleton code for three.

---
+++ lottery.js
@@ -0,0 +1,53 @@
+function Lottery() {
+  this.prize = {};
+  this.fakePrize = {
+    "p1": 1,
+    "p2": 2,
+    "p3": 3
+  }
+
+  this.init = function() {
+    this.prize = this.fakePrize;
+  }
+
+  this.setPrize = function(name, size) {
+    this.prize[name] = size;
+  }
+
+  this.getPrize = function() {
+    return this.prize
+  }
+
+  this.lot = function() {
+    var total,
+        randomIdx,
+        prize;
+
+    total = this.calculateTotalSize();
+
+    // no more prize
+    if(total === 0) {
+      return "No more prize.";
+    }
+
+    randomIdx = total * Math.random();
+
+    for(var name in this.prize) {
+      if(randomIdx < this.prize[name]) {
+        this.prize[name]--;
+        return name;
+      } else
+        randomIdx -= this.prize[name];
+    }
+
+    return null;
+  }
+
+  this.calculateTotalSize = function() {
+    var totalSize = 0;
+    for(var name in this.prize) {
+      totalSize += this.prize[name];
+    }
+    return totalSize;
+  }
+}
